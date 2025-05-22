6785fc93637d14041b36dad3428eb14aab800174
Youssef <ytelmalwany@gmail.com>
2025-05-22 11:21:24 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,6 @@ import Contact from "./components/Contact";
 import Header from "./components/Header";
 import React, { useState, useEffect } from "react";
 function App() {
-   const [position, setPosition] = useState({ x: 0, y: 0 });
-
-  useEffect(() => {
-    const move = (e) => {
-      setPosition({ x: e.clientX, y: e.clientY });
-    };
-    window.addEventListener("mousemove", move);
-
-    return () => {
-      window.removeEventListener("mousemove", move);
-    };
-  }, []);
-
-
-  
   const [currentComponent, setCurrentComponent] = useState("home");
   const handleComponent = () => {
     switch (currentComponent) {
@@ -42,22 +27,6 @@ function App() {
   };
   return (
     <>
-    <div
-      style={{
-        position: "fixed",
-        top: position.y - 40, // عشان تبقى في النص
-        left: position.x - 40,
-        width: 80,
-        height: 80,
-        borderRadius: "50%",
-        border: "3px solid #00adb5",
-        backgroundColor: "transparent",
-        pointerEvents: "none",
-        zIndex: 9999,
-        transition: "top 0.05s linear, left 0.05s linear",
-        mixBlendMode: "difference", // شكلها يبان فوق الخلفية
-      }}
-    ></div>
       <Header setCurrentComponent={setCurrentComponent} />
       {handleComponent()}
     </>
